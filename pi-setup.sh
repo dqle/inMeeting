08ff46ebe7a9940ae49af2c370d2c7d585403698
dqle <dqle123@gmail.com>
2024-08-02 13:42:57 -0700

    chore: add comment to script

--- pi-setup.sh
+++ pi-setup.sh
@@ -1,29 +1,67 @@
 #!/bin/bash
 
-# Enable SPI on your Pi
+echo "----------------------"
+echo "Enable SPI on your Pi"
+echo "----------------------"
+
+echo "raspi-config nonint do_spi 0"
 raspi-config nonint do_spi 0
 
-# Install dependencies
+echo ""
+echo "----------------------"
+echo "Install dependencies"
+echo "----------------------"
 apt-get update
 apt-get install python3 python3-pip git python3-pil
 
-# Allow pip packages to be installed in global context
-sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 
-# git clone inMeeting
+echo ""
+echo "----------------------"
+echo "Allow pip packages to be installed in global context"
+echo "----------------------"
+
+echo "rm /usr/lib/python3.11/EXTERNALLY-MANAGED"
+rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+
+
+echo ""
+echo "----------------------"
+echo "git clone inMeeting"
+echo "----------------------"
+
+echo "git clone https://github.com/dqle/inMeeting.git /home/pi/"
 git clone https://github.com/dqle/inMeeting.git /home/pi/
 
-# Install Piromoni Unicorn Hat Mini python package
+
+echo ""
+echo "----------------------"
+echo "Install Piromoni Unicorn Hat Mini python package"
+echo "----------------------"
+
 cd /home/pi/inMeeting/pi
 echo no | ./unicornhat-mini/install.sh
 
-# Install Python package
+echo ""
+echo "----------------------"
+echo "Install Python package"
+echo "----------------------"
+
 pip3 install -r requirements.txt
 
-# Add program as a service
+
+echo ""
+echo "----------------------"
+echo "Add program as a service"
+echo "----------------------"
+
 cp pi-inmeeting.service /etc/systemd/system/
 systemctl daemon-reload
 
-# Enable and Start service
-systemctl enable pi-attention
-systemctl start pi-attention
+
+echo ""
+echo "----------------------"
+echo "Enable and Start service"
+echo "----------------------"
+
+systemctl enable pi-inmeeting
+systemctl start pi-inmeeting
